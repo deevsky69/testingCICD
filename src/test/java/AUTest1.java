54740ff944e19a34223910ce8731885fb6b648b4
deevsky69 <adivafiqrinugraha69@gmail.com>
2023-02-08 09:45:25 +0700

    first commit

---
+++ src/test/java/AUTest1.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Pluto 07
+ */
+public class AUTest1 {
+    WebDriver driver = null;
+    public AUTest1() {
+    }
+
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void firstTry(){
+        driver.get("https://localhost/index.html");
+        driver.findElement(By.id("no")).click();
+        driver.findElement(By.id("buttoncheck")).click();
+        String expectedResult = "Radio button is checked and it's value is No";
+        String actualResult = driver.findElement(By.className("radiobutton")).getText();
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+    
+    @AfterTest
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
